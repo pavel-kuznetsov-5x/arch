f79993fb4d9d49af479c6080ba0d69e7a2315a07
pavel.kuznetsov <pavel@hypertrack.io>
2022-02-01 22:31:50 +0100

    Initial

---
+++ app/src/main/java/com/example/arch_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.arch_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.FlowPreview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
